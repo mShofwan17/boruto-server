a157f0390063fa0b0bfb3e4732ecbc9a4c295013
mshof <mshofwan.dev@gmail.com>
2023-09-21 19:00:19 +0700

    prod branch

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,9 +2,13 @@ package com.example
 
 import io.ktor.server.application.*
 import com.example.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {

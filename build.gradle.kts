46372f5d30c82520f4b43107dfed4415d46d65bd
mshof <mshofwan.dev@gmail.com>
2023-09-21 19:11:21 +0700

    jar whatever

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,10 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
-tasks.create("stage"){
-    dependsOn("installDist")
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
 }
 
 repositories {
